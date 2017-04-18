c21eebc7266c423f4b254c207efd931ba19eb718
Oros <oros@ecirtam.net>
2017-04-18 18:15:16 +0200

    Merge find_cell_id.py in simple_IMSI-catcher.py

--- simple_IMSI-catcher.py
+++ simple_IMSI-catcher.py
@@ -63,7 +63,13 @@ from optparse import OptionParser
 imsis=[] # [IMSI,...]
 tmsis={} # {TMSI:IMSI,...}
 nb_IMSI=0 # count the number of IMSI
-
+mcc=0
+mnc=0
+lac=0
+cell=0
+country=""
+brand=""
+operator=""
 
 # return something like '0xd9605460'
 def str_tmsi(tmsi):
@@ -119,11 +125,20 @@ def str_imsi(imsi, p=""):
 		print("Error", p, new_imsi, country, brand, operator)
 	return m
 
-# print TMSI, IMSI and operator's information
+# print "Nb IMSI", "TMSI-1", "TMSI-2", "IMSI", "country", "brand", "operator", "MCC", "MNC", "LAC", "CellId"
 def show_imsi(imsi1="", imsi2="", tmsi1="", tmsi2="", p=""):
+	# phones
 	global imsis
 	global tmsis
 	global nb_IMSI
+
+	# cell tower
+	# FIXME : when you change the frequency, this informations is not immediately update.
+	# So you could have wrong values :-/
+	global mcc
+	global mnc
+	global lac
+	global cell
 
 	do_print=False
 	n=''
@@ -169,9 +184,9 @@ def show_imsi(imsi1="", imsi2="", tmsi1="", tmsi2="", p=""):
 
 	if do_print:
 		if imsi1:
-			print("{:7s} ; {:10s} ; {:10s} ; {}".format(str(n), str_tmsi(tmsi1), str_tmsi(tmsi2), str_imsi(imsi1, p)))
+			print("{:7s} ; {:10s} ; {:10s} ; {} ; {:4s} ; {:5s} ; {:6s} ; {:6s}".format(str(n), str_tmsi(tmsi1), str_tmsi(tmsi2), str_imsi(imsi1, p), mcc, mnc, lac, cell))
 		if imsi2:
-			print("{:7s} ; {:10s} ; {:10s} ; {}".format(str(n), str_tmsi(tmsi1), str_tmsi(tmsi2), str_imsi(imsi2, p)))
+			print("{:7s} ; {:10s} ; {:10s} ; {} ; {:4s} ; {:5s} ; {:6s} ; {:6s}".format(str(n), str_tmsi(tmsi1), str_tmsi(tmsi2), str_imsi(imsi2, p), mcc, mnc, lac, cell))
 
 	if not imsi1 and not imsi2 and show_all_tmsi:
 		do_print=False
@@ -182,10 +197,93 @@ def show_imsi(imsi1="", imsi2="", tmsi1="", tmsi2="", p=""):
 			do_print=True
 			tmsis[tmsi2]=""
 		if do_print:
-			print("{:7s} ; {:10s} ; {:10s} ; {}".format(str(n), str_tmsi(tmsi1), str_tmsi(tmsi2), "; ; ;"))
+			print("{:7s} ; {:10s} ; {:10s} ; {} ; {:4s} ; {:5s} ; {:6s} ; {:6s}".format(str(n), str_tmsi(tmsi1), str_tmsi(tmsi2), "; ; ;", mcc, mnc, lac, cell))
+
+
+# return mcc mnc, lac, cell, country, brand, operator
+def find_cell(x):
+	# find_cell() update all following variables
+	global mcc
+	global mnc
+	global lac
+	global cell
+	global country
+	global brand
+	global operator
+
+	"""
+			0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+	0000   00 00 00 00 00 00 00 00 00 00 00 00 08 00 45 00
+	0010   00 43 9a 6b 40 00 40 11 a2 3c 7f 00 00 01 7f 00
+	0020   00 01 ed d1 12 79 00 2f fe 42 02 04 01 00 00 00
+	0030   cc 00 00 07 9b 2c 01 00 00 00 49 06 1b 61 9d 02
+	0040   f8 02 01 9c c8 03 1e 53 a5 07 79 00 00 80 01 40
+	0050   db
+
+	Channel Type: BCCH (1)
+	                          6
+	0030                     01
+
+	Message Type: System Information Type 3
+		                                        c
+	0030                                       1b
+
+	Cell CI: 0x619d (24989)
+		                                           d  e
+	0030                                          61 9d
+
+	Location Area Identification (LAI) - 208/20/412
+	Mobile Country Code (MCC): France (208)	0x02f8
+	Mobile Network Code (MNC): Bouygues Telecom (20) 0xf802
+	Location Area Code (LAC): 0x019c (412)
+			0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+	0030                                                02 
+	0040   f8 02 01 9c
+	"""
+	p=str(x)
+	if ord(p[0x36]) == 0x01: # Channel Type: BCCH (1)
+		if ord(p[0x3c]) == 0x1b: # Message Type: System Information Type 3
+			# FIXME
+			m=hex(ord(p[0x3f]))
+			if len(m)<4:
+				mcc=m[2]+'0'
+			else:
+				mcc=m[3]+m[2]
+			mcc+=str(ord(p[0x40]) & 0x0f)
+
+			# FIXME not works with mnc like 005 or 490
+			m=hex(ord(p[0x41]))
+			if len(m)<4:
+				mnc=m[2]+'0'
+			else:
+				mnc=m[3]+m[2]
+
+			lac=ord(p[0x42])*256+ord(p[0x43])
+			cell=ord(p[0x3d])*256+ord(p[0x3e])
+			brand=""
+			operator=""
+			if mcc in mcc_codes:
+				if mnc in mcc_codes[mcc]['MNC']:
+					country=mcc_codes[mcc]['c'][0]
+					brand=mcc_codes[mcc]['MNC'][mnc][0]
+					operator=mcc_codes[mcc]['MNC'][mnc][1]
+				else:
+					country=mcc_codes[mcc]['c'][0]
+					brand="Unknown"
+					operator=mcc_codes[mcc]['MNC'][mnc][1]
+			mcc=str(mcc)
+			mnc=str(mnc)
+			lac=str(lac)
+			cell=str(cell)
+			country=country.encode('utf-8')
+			brand=brand.encode('utf-8')
+			operator= operator.encode('utf-8')
+			return mcc, mnc, lac, cell, country, brand, operator
+	return None, None, None, None, None, None, None
 
 
 def find_imsi(x):
+	find_cell(x)
 	p=str(x)
 	if ord(p[0x36]) != 0x1: # Channel Type != BCCH (0)
 		tmsi1=""
@@ -295,23 +393,24 @@ def find_imsi(x):
 				show_imsi(imsi1, imsi2, tmsi1, tmsi2, p)
 
 
-parser = OptionParser(usage="%prog: [options]")
-parser.add_option("-a", "--alltmsi", action="store_true", dest="show_all_tmsi", help="Show TMSI who haven't got IMSI (default  : false)")
-parser.add_option("-i", "--iface", dest="iface", default="lo", help="Interface (default : lo)")
-parser.add_option("-m", "--imsi", dest="imsi", default="", type="string", help='IMSI to track (default : None, Example: 123456789101112 or "123 45 6789101112")')
-parser.add_option("-p", "--port", dest="port", default="4729", type="int", help="Port (default : 4729)")
-(options, args) = parser.parse_args()
+if __name__ == '__main__':
+	parser = OptionParser(usage="%prog: [options]")
+	parser.add_option("-a", "--alltmsi", action="store_true", dest="show_all_tmsi", help="Show TMSI who haven't got IMSI (default  : false)")
+	parser.add_option("-i", "--iface", dest="iface", default="lo", help="Interface (default : lo)")
+	parser.add_option("-m", "--imsi", dest="imsi", default="", type="string", help='IMSI to track (default : None, Example: 123456789101112 or "123 45 6789101112")')
+	parser.add_option("-p", "--port", dest="port", default="4729", type="int", help="Port (default : 4729)")
+	(options, args) = parser.parse_args()
 
-show_all_tmsi=options.show_all_tmsi
-imsi_to_track=""
-if options.imsi:
-	imsi="9"+options.imsi.replace(" ", "")
-	for i in range(0,15,2):
-		imsi_to_track+=chr(int(imsi[i+1])*16+int(imsi[i]))
+	show_all_tmsi=options.show_all_tmsi
+	imsi_to_track=""
+	if options.imsi:
+		imsi="9"+options.imsi.replace(" ", "")
+		for i in range(0,15,2):
+			imsi_to_track+=chr(int(imsi[i+1])*16+int(imsi[i]))
 
-# mcc codes form https://en.wikipedia.org/wiki/Mobile_Network_Code
-with open('mcc-mnc/mcc_codes.json', 'r') as file:
-	mcc_codes = json.load(file)
+	# mcc codes form https://en.wikipedia.org/wiki/Mobile_Network_Code
+	with open('mcc-mnc/mcc_codes.json', 'r') as file:
+		mcc_codes = json.load(file)
 
-print("{:7s} ; {:10s} ; {:10s} ; {:17s} ; {} ; {} ; {}".format("Nb IMSI", "TMSI-1", "TMSI-2", "IMSI", "country", "brand", "operator"))
-sniff(iface=options.iface, filter="port {} and not icmp and udp".format(options.port), prn=find_imsi, store=0)
+	print("{:7s} ; {:10s} ; {:10s} ; {:17s} ; {} ; {} ; {} ; {:5s} ; {:4s} ; {:5s} ; {:6s}".format("Nb IMSI", "TMSI-1", "TMSI-2", "IMSI", "country", "brand", "operator", "MCC", "MNC", "LAC", "CellId"))
+	sniff(iface=options.iface, filter="port {} and not icmp and udp".format(options.port), prn=find_imsi, store=0)
